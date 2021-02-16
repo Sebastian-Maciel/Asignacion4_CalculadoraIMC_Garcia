a9299b824580e10a1086843673fa66163291de3b
Sebastián García Maciel <sebastian-maciel@outlook.com>
2021-02-15 21:12:56 -0600

    Se corrigieron errores de compilación

--- app/src/main/java/sebastian/maciel/asignacion4_calculadoraimc_garcia/MainActivity.kt
+++ app/src/main/java/sebastian/maciel/asignacion4_calculadoraimc_garcia/MainActivity.kt
@@ -4,56 +4,56 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import kotlin.random.Random
 
 
 
 class MainActivity : AppCompatActivity() {
+
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var weight:Double = findViewById(R.id.edPeso)
-        var height:Double = findViewById(R.id.edEstatura)
+        var edPeso:EditText = findViewById(R.id.edPeso)
+        var edAltura:EditText = findViewById(R.id.edEstatura)
         var rango:TextView = findViewById(R.id.rango)
         var resultado:TextView = findViewById(R.id.resultado)
-
-
         val botonPresionar: Button = findViewById(R.id.btnCalcular)
 
         botonPresionar.setOnClickListener(){
+        var weight = edPeso.text.toString().toDouble()
+        var height = edAltura.text.toString().toDouble()
+
         var imc: Double = weight / Math.pow(height, 2.0)
+
+
+            resultado.setText(imc.toString())
             if (imc < 18.5){
-                resultado.setText(imc.toString())
                 rango.setText("bajo peso")
-                rango.setBackgroundColor(R.color.colorBrown)
+                rango.setBackgroundResource(R.color.colorGreenish)
             }
-            if (imc >= 18.5 && imc <= 24.9){
-                resultado.setText(imc.toString())
+            if (imc in 18.5..24.9){
                 rango.setText("normal")
-                rango.setBackgroundColor(R.color.colorGreen)
+                rango.setBackgroundResource(R.color.colorGreen)
             }
-            if (imc >= 25 && imc <= 29.9){
-                resultado.setText(imc.toString())
+            if (imc in 25.0..29.9){
                 rango.setText("Sobrepeso")
-                rango.setBackgroundColor(R.color.colorGreenish)
+                rango.setBackgroundResource(R.color.colorBrown)
             }
-            if (imc >= 30 && imc <= 34.9){
-                resultado.setText(imc.toString())
+            if (imc in 30.0..34.9){
                 rango.setText("Obesidad grado 1")
-                rango.setBackgroundColor(R.color.colorOrange)
+                rango.setBackgroundResource(R.color.colorOrange)
             }
-            if (imc >= 35 && imc <= 39.9){
-                resultado.setText(imc.toString())
+            if (imc in 35.0..39.9){
                 rango.setText("Obesidad grado 1")
-                rango.setBackgroundColor(R.color.colorOrange)
+                rango.setBackgroundResource(R.color.colorOrange)
             }
             if (imc >= 40){
-                resultado.setText(imc.toString())
                 rango.setText("Obesidad grado 3")
-                rango.setBackgroundColor(R.color.colorRed)
+                rango.setBackgroundResource(R.color.colorRed)
             }
 
 
